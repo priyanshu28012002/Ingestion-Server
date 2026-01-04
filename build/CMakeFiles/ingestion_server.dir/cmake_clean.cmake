d37f74d958d6627da0f0cd0f0aa16f02d1713db8
priyanshu28012002 <priyanshusrivastav008@gmail.com>
2026-01-04 17:31:08 +0530

    Setup

---
+++ build/CMakeFiles/ingestion_server.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/ingestion_server.dir/src/server.cpp.o"
+  "CMakeFiles/ingestion_server.dir/src/server.cpp.o.d"
+  "bin/ingestion_server"
+  "bin/ingestion_server.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/ingestion_server.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/CMakeFiles/ingestion_server.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for ingestion_server.
+# This may be replaced when dependencies are built.
