6e9c06b47217958931610d61b39efba08d2da2ab
priyanshu28012002 <priyanshusrivastav008@gmail.com>
2026-01-06 11:04:38 +0530

    gst pileline and manager

--- include/gst_pipeline.hpp
+++
@@ -1,9 +0,0 @@
-#ifdef GST_PIPELINE_HPP
-#define GST_PIPELINE_HPP
-
-
-#include 
-
-
-
-#elif // GST_PIPELINE_HPP
--- src/main.cpp
+++ src/main.cpp
@@ -1,28 +1,54 @@
-#include <logger.hpp>
-#include <iostream>
+#include <memory>
 #include <vector>
-#include "camera.hpp"
 
-int main() {
-    int cameraCount = 5;
+#include "camera.hpp"
+#include "video_pipeline.hpp"
+#include "pipeline_manager.hpp"
+#include "logger.hpp"
+
+int main(int argc, char *argv[])
+{
 
     std::vector<Camera> cameras;
-    cameras.reserve(cameraCount);  
+    cameras.reserve(4);
+    LOG_INFO("Start the Server");
 
+    cameras.emplace_back(
+        0,
+        "Camera_" + std::to_string(1),
+        "rtsp://admin:qwerty123@192.168.1.3:554/Streaming/channels/101",
+        "Mera Company");
+    cameras.emplace_back(
+        1,
+        "Camera_" + std::to_string(2),
+        "rtsp://admin:qwerty123@192.168.1.4:554/Streaming/channels/101",
+        "Mera Company");
+    cameras.emplace_back(
+        2,
+        "Camera_" + std::to_string(3),
+        "rtsp://admin:qwerty123@192.168.1.23:554/Streaming/channels/101",
+        "Mera Company");
+    cameras.emplace_back(
+        3,
+        "Camera_" + std::to_string(4),
+        "rtsp://admin:qwerty123@192.168.1.12:554/stream2",
+        "Mera Company");
 
-    //Read the Json File and get the meta data of the camera and make the camera instance
-    for (int i = 0; i < cameraCount; ++i) {
-        cameras.emplace_back(
-            i,
-            "Camera_" + std::to_string(i),
-            "rtsp://192.168.1." + std::to_string(100 + i) + "/stream" , // replace the URL 
-            "Mera Comapny"
-        );
+    LOG_INFO("Configer Camera"); 
+    PipelineManager manager;
+
+    for (const auto &cam : cameras)
+    {
+        manager.add_pipeline(
+            std::make_unique<VideoPipeline>(cam));
     }
 
+    LOG_INFO("Configer Pipeline");
+    LOG_INFO("Started Video Stream");
+
+    manager.run();
     
-    std::cout << "Created " << cameras.size()
-              << " camera objects\n";
+    LOG_INFO("Closed Video Stream");
 
     return 0;
 }
